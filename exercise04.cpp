af2fc8ccf57166434fa0cbae612908bc9a26fdf5
Madhuwantha G K O <110708704+IT21802058@users.noreply.github.com>
2022-08-15 19:19:31 +0000

    Reference Type Parameters

--- exercise04.cpp
+++ exercise04.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void print(int len, int wth);
 void input(int len, int wth);
 
-// Do not change the main() function
+
 int main() {
    int length = 10, width = 5;
    input(length, width);
@@ -12,10 +12,15 @@ int main() {
    return 0;
 }
 
-// Do not change the print() function
+
 void print(int len, int wth) {
    cout << "Length : " << len 
         << ", Width  : " << wth << endl;
 }
 
-// Implement the Input Function here
+void input(int &len, int &wth){
+	cout << "input the length :";
+	cin >> len;
+	cout << "input the width :";
+	cin >> wth;
+}
